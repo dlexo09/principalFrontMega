1b14383ce7c662ab7538a4682b3e95a4ac8d9e3d
Jessy-07 <jeortegam@megacable.com.mx>
2025-01-09 17:57:02 -0600

    comienso de la seccion disneyplus

---
+++ src/components/DestacadosStreaming.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './DestacadosStreaming.css';
+
+const DestacadosStreaming = () => {
+    return (
+        <>
+        <div className="destacados-streamings-container">
+            <div className="destacados-streaming-txt text-center">
+                <p><span>Disney+</span>, un lugar único para disfrutar las mejores historias de <span>Disney, Pixar, Marvel, Star Wars, National Geographic</span>,
+            el deporte de <span>ESPN</span> y las series y películas de <span>Star.</span></p>
+            </div>
+            <div className="destacados-streaming-img container-fluid">
+                <img className='w-100' src="../src/assets/images/streamings/disneyplus/destacados-disney.png" alt="" />
+            </div>
+        </div>
+        </>
+    );
+};
+export default DestacadosStreaming;
