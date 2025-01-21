c4785c8ddff4337209991513ae712fed24c0ed34
Jessy-07 <jeortegam@megacable.com.mx>
2025-01-21 17:52:55 -0600

    Ajustes en los subenlaces al entrar a Television

--- src/views/Television.jsx
+++ src/views/Television.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom"; // Importa Navigate
 import TabsComponent from "../components/TabsComponent";
 import TVConoceServicio from "../components/TVConoceServicio";
 import TVCanales from "../components/TVCanales";
@@ -13,7 +13,7 @@ const Television = () => {
 
   return (
     <div className="container-fluid p-0">
-        <h1 className="text-center title-tabs">TV INTERACTIVA</h1>
+      <h1 className="text-center title-tabs">TV INTERACTIVA</h1>
       {/* Componente de tabs reutilizable */}
       <TabsComponent tabs={tabs} basePath="/television" />
 
@@ -21,6 +21,8 @@ const Television = () => {
         {/* Renderizamos el contenido de las subrutas */}
         <div className="tab-content mt-4">
           <Routes>
+            {/* Redirige a la tab inicial si no se especifica ninguna subruta */}
+            <Route path="/" element={<Navigate to="conoce-nuestro-servicio" />} />
             <Route path="conoce-nuestro-servicio" element={<TVConoceServicio />} />
             <Route path="canales" element={<TVCanales />} />
           </Routes>
