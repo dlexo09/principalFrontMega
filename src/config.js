f4dc99e51f0fbf1cf8c224e0c0ceb6f346781709
Alexis Gonzalez <dlexo09@gmail.com>
2025-01-29 17:15:23 -0600

    Cambiamos el title y el svg de la pestaña de mega

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
-export const serverUrl = 'http://localhost:5173/';
-export const serverAPIUrl = 'http://localhost:4000/';
+export const serverUrl = 'http://localhost:5173/'; //https://dpp40vri69wj3.cloudfront.net/ s3 bucket
+export const serverAPIUrl = 'http://localhost:4000/'; //lambda api gateway
