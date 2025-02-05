5d2449fe46eb9cf28ab948491c7f861e2c2e9ed6
Jessica <jessiica_om97@hotmail.com>
2025-02-05 16:09:21 -0600

    Continuación de secciones internas de AYUDA

---
+++ src/components/SOPCIS.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./SOPCIS.css";
+import "./Globales.css";
+
+const TICostos = () => {
+  return (
+    <div className="general-tabs-container">
+      <div className="text-center">
+        <h3 className="small-title-services">CONOCE EL MAPA INTERACTIVO</h3>
+        <h2 className="big-title-services">¡Ubica tu CIS más cercano!</h2>
+      </div>
+    </div>
+  );
+};
+
+export default TICostos;
--- src/views/Ayuda.jsx
+++ src/views/Ayuda.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom"; // Importa Navigate
 import TabsComponent from "../components/TabsComponent";
 import SOPPago from "../components/SOPPago";
+import SOPFAQ from "../components/SOPFAQ";
+import SOPCIS from "../components/SOPCIS";
+import SoporteOnline from "../components/SoporteOnline";
 import '../components/Globales.css';
 
-const Internet = () => {
+const Ayuda = () => {
   const tabs = [
     { id: "formas-de-pago", label: "Formas de pago" },
-
+    { id: "preguntas-frecuentes", label: "Preguntas Frecuentes" },
+    { id: "centros-de-atencion", label: "CIS" },
+    { id: "soporte-tecnico-online", label: "Soporte Técnico" },
   ];
 
   return (
     <div className="container-fluid p-0">
-      <h1 className="text-center title-tabs">SOPORTE</h1>
+      <h1 className="text-center title-tabs">AYUDA</h1>
       {/* Componente de tabs reutilizable */}
       <TabsComponent tabs={tabs} basePath="/ayuda" />
 
@@ -23,6 +28,9 @@ const Internet = () => {
             {/* Redirige a la tab inicial si no se especifica ninguna subruta */}
             <Route path="/" element={<Navigate to="formas-de-pago" />} />
             <Route path="formas-de-pago" element={<SOPPago />} />
+            <Route path="preguntas-frecuentes" element={<SOPFAQ />} />
+            <Route path="centros-de-atencion" element={<SOPCIS />} />
+            <Route path="soporte-tecnico-online" element={<SoporteOnline />} />
           </Routes>
         </div>
       </div>
@@ -30,4 +38,4 @@ const Internet = () => {
   );
 };
 
-export default Internet;
+export default Ayuda;
