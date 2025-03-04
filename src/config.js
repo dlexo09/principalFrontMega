5fa05a9fc0f2e2cb69f934223f1989e8af6d02d8
Jessy-07 <jeortegam@megacable.com.mx>
2025-03-04 12:09:53 -0600

    cambio config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 export const serverUrl = 'http://localhost:5173/'; //https://dpp40vri69wj3.cloudfront.net/ s3 bucket
-export const serverAPIUrl = 'http://localhost:3000/dev/'; //server ec2
-export const serverAPILambda = 'https://oz2422gq6i.execute-api.us-east-1.amazonaws.com/'; //lambda api gateway
+export const serverAPIUrl = 'http://localhost:4000/'; //server ec2
+export const serverAPILambda = 'http://localhost:4000/'; //lambda api gateway
 
 
 // https://dpp40vri69wj3.cloudfront.net/  s3
