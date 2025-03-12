8e26f8c1ab2ef39d7eab2398cee0c7f620146e0e
Alexis Gonzalez <dlexo09@gmail.com>
2025-03-12 10:37:30 -0600

    se agrega una api para el navbar que decide si ex sview xview+ o television

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,61 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { serverUrl, serverAPILambda } from '../config'; // Ajusta la ruta según la ubicación de tu archivo config.js
+import { LocationContext } from '../LocationContext';
 import './NavBar.css';
-import { serverUrl } from '../config'; // Ajusta la ruta según la ubicación de tu archivo config.js
-
 
 const NavBar = () => {
+  const { currentLocation } = useContext(LocationContext);
+  const [paqueteData, setPaqueteData] = useState([]);
+  const idSucursal = currentLocation?.idSucursal;
+
+  useEffect(() => {
+    const fetchPaqueteData = async () => {
+      if (idSucursal) {
+        try {
+          const response = await fetch(`${serverAPILambda}api/xview/${idSucursal}`);
+          const data = await response.json();
+          setPaqueteData(data);
+        } catch (error) {
+          console.error('Error fetching paquete data:', error);
+        }
+      }
+    };
+
+    fetchPaqueteData();
+  }, [idSucursal]);
+
+  const getLinkXview = () => {
+    if (paqueteData.some(paquete => paquete.idTipoPaquete === 4)) {
+      return '/xviewplus';
+    } else if (paqueteData.some(paquete => paquete.idTipoPaquete === 3)) {
+      return '/xview';
+    } else {
+      return '/tv';
+    }
+  };
+  const getTextXview = () => {
+    if (paqueteData.some(paquete => paquete.idTipoPaquete === 4)) {
+      return 'Xview+';
+    } else if (paqueteData.some(paquete => paquete.idTipoPaquete === 3)) {
+      return 'Xview';
+    } else {
+      return 'Televisión';
+    }
+  };
+
   return (
     <nav className="navbar navbar-expand-xl navbar-custom navbar-container">
       <div className="container">
         {/* Logo */}
         <div className="navbar-logo">
-        <a href={serverUrl}>
-          <img
-            src="../src/assets/images/general/mega-logo.png"
-            alt="Logo"
-            className="logo-img"
-          />
-        </a>
-      </div>
+          <a href={serverUrl}>
+            <img
+              src="../src/assets/images/general/mega-logo.png"
+              alt="Logo"
+              className="logo-img"
+            />
+          </a>
+        </div>
         <button
           className="navbar-toggler"
           type="button"
@@ -29,7 +69,6 @@ const NavBar = () => {
         </button>
         <div className="collapse navbar-collapse justify-content-end mt-4" id="navbarNav">
           <ul className="navbar-nav">
-            
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Oferta
@@ -41,11 +80,11 @@ const NavBar = () => {
               </ul>
             </li>
             <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle"  id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Servicios
               </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">      
-                <li><a className="dropdown-item" href="/television">Televisión</a></li>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a className="dropdown-item" href={getLinkXview()}>{getTextXview()}</a></li>
                 <li><a className="dropdown-item" href="/internet">Internet</a></li>
                 <li><a className="dropdown-item" href="/telefonia-ilimitada">Telefonía Ilimitada</a></li>
               </ul>
@@ -66,31 +105,24 @@ const NavBar = () => {
               <a className="nav-link" aria-current="page" href="/fibra-optica">Fibra óptica y simetria</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="/xviewplus">Xview+</a>
+              <a className="nav-link" aria-current="page" href={getLinkXview()}>{getTextXview()}</a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="/xview">Xview</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="/tv">TV</a>
-            </li>
+        
             <li className="nav-item">
               <a className="nav-link" aria-current="page">Mega móvil</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" aria-current="page" href="/ayuda">Ayuda</a>
-            </li> 
+            </li>
             {/* Movil */}
             <li className="nav-item">
               <a className="nav-link d-xl-none" target='_blank' aria-current="page" href="https://pagoenlinea.megacable.com.mx/">Paga en Línea</a>
-            </li> 
+            </li>
             <li className="nav-item">
               <a className="nav-link d-xl-none" target='_blank' aria-current="page" href="https://sel.megacable.com.mx/">Mi cuenta Mega</a>
-            </li> 
+            </li>
           </ul>
         </div>
-
-
       </div>
     </nav>
   );
