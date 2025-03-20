fc51eebb1766a2cc0275c20d2255bc54f18a201f
Alexis Gonzalez <dlexo09@gmail.com>
2025-03-20 14:04:54 -0600

    refactorizacion de codigo y modificacion a trivias desde api serverless

--- src/views/TriviaDetail.jsx
+++ src/views/TriviaDetail.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { serverAPIUrl } from "../config"; // Ajusta la ruta según la ubicación de tu archivo config.js
+import { serverAPILambda } from "../config"; // Ajusta la ruta según la ubicación de tu archivo config.js
 import "./TriviaDetail.css";
 import ActivarCuenta from "./ActivarCuenta";
 
@@ -11,33 +9,42 @@ const TriviaDetail = () => {
   const [trivia, setTrivia] = useState(null);
   const [preguntas, setPreguntas] = useState([]);
   const [selectedOptions, setSelectedOptions] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchTrivia = async () => {
+    const fetchTriviaData = async () => {
       try {
-        const response = await fetch(
-          `${serverAPIUrl}api/trivias/data?id=${endpoint}`
-        );
-        const data = await response.json();
-        setTrivia(data);
-        console.log(data);
-
-        if (data.idTriviaConfig) {
-          // Usar idtriviaConfig para obtener las preguntas
-          const preguntasResponse = await fetch(
-            `${serverAPIUrl}api/triviaspreguntas/data?id=${data.idTriviaConfig}`
-          );
-          const preguntasData = await preguntasResponse.json();
-          setPreguntas(preguntasData);
+        // Llamada a la primera API para obtener los datos de la trivia
+        const triviaResponse = await fetch(`${serverAPILambda}api/trivias/data?id=${endpoint}`);
+        console.log(triviaResponse);
+        const triviaDataArray = await triviaResponse.json();
+    
+        if (triviaDataArray.length > 0) {
+          const triviaData = triviaDataArray[0]; // Acceder al primer elemento del array
+          setTrivia(triviaData);
+    
+          // Esperar a que la trivia cargue antes de llamar a la segunda API
+          if (triviaData.idTriviaConfig) {
+            const preguntasResponse = await fetch(
+              `${serverAPILambda}api/triviaspreguntas/data?id=${triviaData.idTriviaConfig}`
+            );
+            const preguntasData = await preguntasResponse.json();
+            setPreguntas(preguntasData);
+          } else {
+            console.error("idTriviaConfig is undefined");
+          }
         } else {
-          console.error("idtriviaConfig is undefined");
+          console.error("No se encontraron datos de trivia.");
+          setTrivia(null);
         }
       } catch (error) {
         console.error("Error fetching trivia or preguntas:", error);
+      } finally {
+        setLoading(false); // Finalizar la carga
       }
     };
 
-    fetchTrivia();
+    fetchTriviaData();
   }, [endpoint]);
 
   const handleOptionChange = (preguntaId, opcion) => {
@@ -47,205 +54,166 @@ const TriviaDetail = () => {
     }));
   };
 
-  if (!trivia) {
+  if (loading) {
     return <div>Loading...</div>;
+  }
+
+  if (!trivia) {
+    return <div>Error: No se pudo cargar la trivia.</div>;
   }
 
   return (
     <ActivarCuenta mainTitle="PARTICIPA" title="Y GANA">
       <div className="pt-5 trivias-container">
-        <div className="text-center ps-4 pe-4 ps-lg-0 pe-lg-0">
-          <h2 className="small-title-services">GANA INCREÍBLES PREMIOS</h2>
-          <h3 className="big-title-services big-title-container-sm">
-            ¡Participa, Regístrate y Responde las Preguntas para Ganar!
-          </h3>
-        </div>
-
-        <div className="row mt-5">
-          <div className="col-12 align-items-center d-flex ps-4 pe-4 ps-lg-0 pe-lg-0">
-            <img
-              src={`${serverAPIUrl}${trivia.rutaPrincipal}${trivia.archivoPrincipal}`}
-              className="trivia-banner img-fluid d-none d-md-block"
-              alt="Trivia Banner"
-            />
-            {/* Imagen para mobil */}
-            <img src="../src/assets/uploads/bannerTrivias/banner-trivias-mov.png" className="w-100 d-md-none" alt="" />
-          </div>
-
-          <h1>{trivia.titulo}</h1>
-          <p>{trivia.descripcion}</p>
-
-          <form>
-            <div className="datos-trivia ps-4 pe-4 ps-lg-0 pe-lg-0">
-              <div className="text-center mb-5">
-                <h3 className="big-title-services text-center">
-                  Datos personales
-                </h3>
-                <p>Completa los datos antes de continuar</p>
-              </div>
-              <div className="container datos-personales-trivia">
-                <div className="row ">
-                  <div className="col-md-6 ">
-                    {/* Aquí puedes agregar los campos del formulario según sea necesario */}
-                    <div className="mb-3">
-                      <label htmlFor="nombre" className="form-label form-label-datos">
-                        Nombre completo
-                      </label>
-                      <input
-                        type="text"
-                        className="form-control activar-cuenta_input"
-                        id="nombre"
-                        name="nombre"
-                        required
-                      />
-                    </div>
-
-                    <div className="mb-3">
-                      <label htmlFor="telefono" className="form-label form-label-datos">
-                        Teléfono
-                      </label>
-                      <input
-                        type="number"
-                        className="form-control activar-cuenta_input "
-                        id="telefono"
-                        name="telefono"
-                        required
-                      />
-                    </div>
-
-                    <div className="mb-3">
-                      <label htmlFor="titular" className="form-label form-label-datos">
-                        Nombre del titular del contrato
-                      </label>
-                      <input
-                        type="text"
-                        className="form-control activar-cuenta_input"
-                        id="titular"
-                        name="titular"
-                        required
-                      />
-                    </div>
-                  </div>
-
-                  <div className="col-md-6">
-                    {/* Aquí puedes agregar los campos del formulario según sea necesario */}
-                    <div className="mb-3">
-                      <label htmlFor="edad" className="form-label form-label-datos">
-                        Edad
-                      </label>
-                      <input
-                        type="number"
-                        className="form-control activar-cuenta_input"
-                        id="edad"
-                        name="edad"
-                        required
-                      />
-                    </div>
-
-                    <div className="mb-3">
-                      <label htmlFor="contrato" className="form-label form-label-datos">
-                        Número de contrato
-                      </label>
-                      <input
-                        type="text"
-                        className="form-control activar-cuenta_input"
-                        id="contrato"
-                        name="contrato"
-                        required
-                      />
-                    </div>
-
-                    <div className="mb-3">
-                      <label htmlFor="ciudad" className="form-label form-label-datos">
-                        Ciudad
-                      </label>
-                      <input
-                        type="text"
-                        className="form-control activar-cuenta_input"
-                        id="ciudad"
-                        name="ciudad"
-                        required
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            {/* ******** TRIVIA ******** */}
-            <div className="container datos-personales-trivia">
-              <div className="text-center mb-3 mb-lg-5">
-                <h3 className="big-title-services text-center">GIVE AWAY</h3>
-              </div>
-              <div className="col-12">
-                <div className="container">
-                  {preguntas.map((pregunta, index) => (
-                    <div key={index} className="mb-3">
-                      <label className="form-label">{pregunta.pregunta}</label>
-                      {pregunta.tipoPregunta === 2 ? (
-                        <input
-                          type="text"
-                          className="form-control form-open-faq"
-                          name={`pregunta_${pregunta.idTriviasPreguntas}`}
-                          required
-                        />
-                      ) : (
-                        <div className="trivia-options">
-                          {[pregunta.a, pregunta.b, pregunta.c, pregunta.d].map(
-                            (opcion, idx) => (
-                              <div
-                                key={idx}
-                                className={`form-check ${
-                                  selectedOptions[pregunta.idTriviasPreguntas] === opcion
-                                    ? "active"
-                                    : ""
-                                }`}
-                                onClick={() =>
-                                  handleOptionChange(pregunta.idTriviasPreguntas, opcion)
-                                }
-                              >
-                                <input
-                                  className="form-check-input"
-                                  type="radio"
-                                  name={`pregunta_${pregunta.idTriviasPreguntas}`}
-                                  id={`pregunta_${pregunta.idTriviasPreguntas}_opcion_${idx}`}
-                                  value={opcion}
-                                  checked={
-                                    selectedOptions[pregunta.idTriviasPreguntas] === opcion
-                                  }
-                                  onChange={() =>
-                                    handleOptionChange(pregunta.idTriviasPreguntas, opcion)
-                                  }
-                                  required
-                                />
-                                <label
-                                  className="form-check-label"
-                                  htmlFor={`pregunta_${pregunta.idTriviasPreguntas}_opcion_${idx}`}
-                                >
-                                  {opcion}
-                                </label>
-                              </div>
-                            )
-                          )}
-                        </div>
-                      )}
-                    </div>
-                  ))}
-                </div>
-              </div>
-              
-              <div className="text-center mt-5">
-              <button type="submit" className="btn btn-packs">
-                Enviar
-              </button>
-              </div>
-              
-            </div>
-          </form>
-        </div>
+        <Header trivia={trivia} />
+        <TriviaForm
+          trivia={trivia}
+          preguntas={preguntas}
+          selectedOptions={selectedOptions}
+          handleOptionChange={handleOptionChange}
+        />
       </div>
     </ActivarCuenta>
   );
 };
+
+// Componente para el encabezado
+const Header = ({ trivia }) => {
+  if (!trivia || !trivia.bannerPrincipal || !trivia.bannerMovil) {
+    return null; // No renderizar nada si los datos no están disponibles
+  }
+
+  return (
+    <div className="text-center ps-4 pe-4 ps-lg-0 pe-lg-0">
+      <h2 className="small-title-services">GANA INCREÍBLES PREMIOS</h2>
+      <h3 className="big-title-services big-title-container-sm">
+        ¡Participa, Regístrate y Responde las Preguntas para Ganar!
+      </h3>
+      <div className="row mt-5">
+        <div className="col-12 align-items-center d-flex ps-4 pe-4 ps-lg-0 pe-lg-0">
+          {/* Imagen del banner principal */}
+          <img
+            src={`/uploads/bannerTrivias/${trivia.bannerPrincipal}`}
+            className="trivia-banner img-fluid d-none d-md-block"
+            alt="Trivia Banner"
+          />
+          {/* Imagen del banner móvil */}
+          <img
+            src={`/uploads/bannerTrivias/${trivia.bannerMovil}`}
+            className="w-100 d-md-none"
+            alt="Trivia Banner Mobile"
+          />
+        </div>
+        {/* Título y descripción de la trivia */}
+        <h1>{trivia.titulo}</h1>
+        <p>{trivia.descripcion}</p>
+      </div>
+    </div>
+  );
+};
+// Componente para el formulario de trivia
+const TriviaForm = ({ trivia, preguntas, selectedOptions, handleOptionChange }) => (
+  <form>
+    <PersonalDataForm />
+    <TriviaQuestions
+      preguntas={preguntas}
+      selectedOptions={selectedOptions}
+      handleOptionChange={handleOptionChange}
+    />
+    <div className="text-center mt-5">
+      <button type="submit" className="btn btn-packs">
+        Enviar
+      </button>
+    </div>
+  </form>
+);
+
+// Componente para los datos personales
+const PersonalDataForm = () => (
+  <div className="datos-trivia ps-4 pe-4 ps-lg-0 pe-lg-0">
+    <div className="text-center mb-5">
+      <h3 className="big-title-services text-center">Datos personales</h3>
+      <p>Completa los datos antes de continuar</p>
+    </div>
+    <div className="container datos-personales-trivia">
+      <div className="row">
+        <div className="col-md-6">
+          <InputField label="Nombre completo" id="nombre" type="text" />
+          <InputField label="Teléfono" id="telefono" type="number" />
+          <InputField label="Nombre del titular del contrato" id="titular" type="text" />
+        </div>
+        <div className="col-md-6">
+          <InputField label="Edad" id="edad" type="number" />
+          <InputField label="Número de contrato" id="contrato" type="text" />
+          <InputField label="Ciudad" id="ciudad" type="text" />
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+// Componente para las preguntas de la trivia
+const TriviaQuestions = ({ preguntas, selectedOptions, handleOptionChange }) => (
+  <div className="container datos-personales-trivia">
+    <div className="text-center mb-3 mb-lg-5">
+      <h3 className="big-title-services text-center">GIVE AWAY</h3>
+    </div>
+    <div className="col-12">
+      <div className="container">
+        {preguntas.map((pregunta, index) => (
+          <div key={index} className="mb-3">
+            <label className="form-label">{pregunta.pregunta}</label>
+            {pregunta.tipoPregunta === 2 ? (
+              <input
+                type="text"
+                className="form-control form-open-faq"
+                name={`pregunta_${pregunta.idTriviasPreguntas}`}
+                required
+              />
+            ) : (
+              <div className="trivia-options">
+                {[pregunta.a, pregunta.b, pregunta.c, pregunta.d].map((opcion, idx) => (
+                  <div
+                    key={idx}
+                    className={`form-check ${selectedOptions[pregunta.idTriviasPreguntas] === opcion ? "active" : ""
+                      }`}
+                    onClick={() => handleOptionChange(pregunta.idTriviasPreguntas, opcion)}
+                  >
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name={`pregunta_${pregunta.idTriviasPreguntas}`}
+                      id={`pregunta_${pregunta.idTriviasPreguntas}_opcion_${idx}`}
+                      value={opcion}
+                      checked={selectedOptions[pregunta.idTriviasPreguntas] === opcion}
+                      onChange={() => handleOptionChange(pregunta.idTriviasPreguntas, opcion)}
+                      required
+                    />
+                    <label
+                      className="form-check-label"
+                      htmlFor={`pregunta_${pregunta.idTriviasPreguntas}_opcion_${idx}`}
+                    >
+                      {opcion}
+                    </label>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  </div>
+);
+
+// Componente reutilizable para los campos de entrada
+const InputField = ({ label, id, type }) => (
+  <div className="mb-3">
+    <label htmlFor={id} className="form-label form-label-datos">
+      {label}
+    </label>
+    <input type={type} className="form-control activar-cuenta_input" id={id} name={id} required />
+  </div>
+);
 
 export default TriviaDetail;
--- src/views/Trivias.jsx
+++ src/views/Trivias.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { serverAPIUrl, serverAPILambda, serverUrl } from "../config"; // Ajusta la ruta según la ubicación de tu archivo config.js
+import { serverAPILambda } from "../config"; // Ajusta la ruta según la ubicación de tu archivo config.js
 import "./Trivias.css";
 import ActivarCuenta from "./ActivarCuenta";
 
@@ -15,20 +13,14 @@ const Trivias = () => {
         const response = await fetch(`${serverAPILambda}api/trivias/`);
         const data = await response.json();
         const now = new Date();
-        const today = new Date(
-          now.getFullYear(),
-          now.getMonth(),
-          now.getDate()
-        );
+        const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+
         const activeBanners = data.filter((banner) => {
-          const startDate = new Date(
-            new Date(banner.fhInicio).setHours(0, 0, 0, 0)
-          );
-          const endDate = new Date(
-            new Date(banner.fhFin).setHours(23, 59, 59, 999)
-          );
+          const startDate = new Date(new Date(banner.fhInicio).setHours(0, 0, 0, 0));
+          const endDate = new Date(new Date(banner.fhFin).setHours(23, 59, 59, 999));
           return banner.status === 1 && startDate <= today && endDate >= today;
         });
+
         setBanners(activeBanners);
       } catch (error) {
         console.error("Error fetching banners:", error);
@@ -38,103 +30,136 @@ const Trivias = () => {
     fetchBanners();
   }, []);
 
-  const handleSlideChange = (index) => {
-    setActiveIndex(index);
-  };
+  useEffect(() => {
+    const carouselElement = document.getElementById("carouselTrivias");
+
+    const handleSlide = (event) => {
+      const newIndex = parseInt(event.to, 10);
+      setActiveIndex(newIndex);
+    };
+
+    // Escuchar el evento `slid.bs.carousel`
+    carouselElement.addEventListener("slid.bs.carousel", handleSlide);
+
+    return () => {
+      // Limpiar el evento al desmontar el componente
+      carouselElement.removeEventListener("slid.bs.carousel", handleSlide);
+    };
+  }, []);
 
   return (
     <ActivarCuenta mainTitle="PARTICIPA" title="Y GANA">
       <div className="container pt-5 trivias-container">
-        <div className="text-center">
-          <h2 className="small-title-services">SOLO PARA VERDADEROS FANS</h2>
-          <h3 className="big-title-services big-title-container-sm">
-            Escoge una de nuestras trivias disponibles y prueba tus conocimientos
-          </h3>
-        </div>
-
-        <div
-          id="carouselTrivias"
-          className="carouselTrivias carousel slide mt-5"
-          data-bs-ride="carousel"
-          onSlide={(e) => handleSlideChange(e.to)}
-        >
-          <div className="carousel-indicators">
-            {banners.map((banner, index) => (
-              <button
-                key={index}
-                type="button"
-                data-bs-target="#carouselTrivias"
-                data-bs-slide-to={index}
-                className={index === 0 ? "active" : ""}
-                aria-current={index === 0 ? "true" : "false"}
-                aria-label={`Slide ${index + 1}`}
-                onClick={() => handleSlideChange(index)}
-              ></button>
-            ))}
-          </div>
-          <div className="carousel-inner carrousel-trivias">
-            {banners.map((banner, index) => (
-              <div
-                key={index}
-                className={`carousel-item ${index === 0 ? "active" : ""}`}
-              >
-                <img
-                  src={`${serverUrl}src/assets/uploads/bannerTrivias/${banner.archivoPrincipal}`}
-                  className="d-block w-100 d-none d-md-block"
-                  alt={`Slide ${index + 1}`}
-                />
-                {/* Imagen para mobil */}
-                <img src="../src/assets/uploads/bannerTrivias/banner-trivias-mov.png" className="w-100 d-md-none" alt="" />
-              
-              </div>
-            ))}
-          </div>
-          <button
-            className="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselTrivias"
-            data-bs-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button
-            className="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselTrivias"
-            data-bs-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Next</span>
-          </button>
-        </div>
-
-        {banners[activeIndex] && banners[activeIndex].urlEndPoint && (
-          <div className="text-center mt-5">
-            <a
-              href={`/trivias/${banners[activeIndex].urlEndPoint}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="btn-action btn-trivias"
-            >
-              Participa Ahora
-              <span className="open-page-icon"></span>
-            </a>
-          </div>
-        )}
-
-<div className="text-center legal-trivia">
-        <p>Ponemos a su disposición la actualización de nuestro aviso de privacidad <a className='txt-prime-color' target='_blank' href="https://www.megacable.com.mx/aviso-de-privacidad">Aquí</a></p>
-    </div>
+        <Header />
+        <TriviaCarousel banners={banners} activeIndex={activeIndex} />
+        <ParticipateButton banners={banners} activeIndex={activeIndex} />
+        <LegalNotice />
       </div>
     </ActivarCuenta>
   );
 };
+
+// Componente para el encabezado
+const Header = () => (
+  <div className="text-center">
+    <h2 className="small-title-services">SOLO PARA VERDADEROS FANS</h2>
+    <h3 className="big-title-services big-title-container-sm">
+      Escoge una de nuestras trivias disponibles y prueba tus conocimientos
+    </h3>
+  </div>
+);
+
+// Componente para el carrusel de trivias
+const TriviaCarousel = ({ banners, activeIndex }) => (
+  <div id="carouselTrivias" className="carouselTrivias carousel slide mt-5" data-bs-ride="carousel">
+    <div className="carousel-indicators">
+      {banners.map((_, index) => (
+        <button
+          key={index}
+          type="button"
+          data-bs-target="#carouselTrivias"
+          data-bs-slide-to={index}
+          className={index === activeIndex ? "active" : ""}
+          aria-current={index === activeIndex ? "true" : "false"}
+          aria-label={`Slide ${index + 1}`}
+        ></button>
+      ))}
+    </div>
+    <div className="carousel-inner carrousel-trivias">
+      {banners.map((banner, index) => (
+        <div key={index} className={`carousel-item ${index === activeIndex ? "active" : ""}`}>
+          <img
+            src={`/uploads/bannerTrivias/${banner.bannerPrincipal}`}
+            className="d-block w-100 d-none d-md-block"
+            alt={`Slide ${index + 1}`}
+          />
+          <img
+            src={`/uploads/bannerTrivias/${banner.bannerMovil}`}
+            className="w-100 d-md-none"
+            alt={`Slide ${index + 1}`}
+          />
+        </div>
+      ))}
+    </div>
+    <button
+      className="carousel-control-prev"
+      type="button"
+      data-bs-target="#carouselTrivias"
+      data-bs-slide="prev"
+    >
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Previous</span>
+    </button>
+    <button
+      className="carousel-control-next"
+      type="button"
+      data-bs-target="#carouselTrivias"
+      data-bs-slide="next"
+    >
+      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Next</span>
+    </button>
+  </div>
+);
+
+// Componente para el botón "Participa Ahora"
+const ParticipateButton = ({ banners, activeIndex }) => {
+  const activeBanner = banners[activeIndex];
+
+  if (!activeBanner || !activeBanner.urlEndPoint) {
+    return null;
+  }
+
+  return (
+    <div className="text-center mt-5">
+      <a
+        href={`/trivias/${activeBanner.urlEndPoint}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="btn-action btn-trivias"
+      >
+        Participa Ahora
+        <span className="open-page-icon"></span>
+      </a>
+    </div>
+  );
+};
+
+// Componente para el aviso legal
+const LegalNotice = () => (
+  <div className="text-center legal-trivia">
+    <p>
+      Ponemos a su disposición la actualización de nuestro aviso de privacidad{" "}
+      <a
+        className="txt-prime-color"
+        target="_blank"
+        href="https://www.megacable.com.mx/aviso-de-privacidad"
+        rel="noopener noreferrer"
+      >
+        Aquí
+      </a>
+    </p>
+  </div>
+);
 
 export default Trivias;
