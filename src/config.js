e6964bc8c40ce134614f837e51c0a83db4f5ea87
Alexis Gonzalez <dlexo09@gmail.com>
2025-04-11 13:33:45 -0600

    config para local pero apis en aws gateway

--- src/config.js
+++ src/config.js
@@ -1,7 +1,12 @@
 // src/config.js
-export const serverUrl = 'http://localhost:5173/'; //https://dpp40vri69wj3.cloudfront.net/ s3 bucket
-export const serverAPIUrl = 'http://localhost:4000/'; //server ec2
-export const serverAPILambda = 'http://localhost:3000/dev/'; //lambda api gateway
+export const serverUrl = 'https://dpp40vri69wj3.cloudfront.net/'; //https://dpp40vri69wj3.cloudfront.net/ s3 bucket
+export const serverAPIUrl = 'https://oz2422gq6i.execute-api.us-east-1.amazonaws.com/'; //server ec2
+export const serverAPILambda = 'https://oz2422gq6i.execute-api.us-east-1.amazonaws.com/'; //lambda api gateway
+
+// src/config.js
+// export const serverUrl = 'http://localhost:5173/'; //https://dpp40vri69wj3.cloudfront.net/ s3 bucket
+// export const serverAPIUrl = 'http://localhost:4000/'; //server ec2
+// export const serverAPILambda = 'http://localhost:3000/dev/'; //lambda api gateway
 
 
 // https://dpp40vri69wj3.cloudfront.net/  s3
